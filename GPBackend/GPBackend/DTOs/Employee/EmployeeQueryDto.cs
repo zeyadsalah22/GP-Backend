984d24969d2a53f8b2d35e6526d713d815a8fd6f
Aliaa Hamad <125764832+Aliaaxx@users.noreply.github.com>
2025-05-21 18:28:44 +0300

    Create EmployeeQueryDto.cs

---
+++ GPBackend/GPBackend/DTOs/Employee/EmployeeQueryDto.cs
@@ -0,0 +1,11 @@
+using GPBackend.DTOs.Common;
+
+namespace GPBackend.DTOs.Employee
+{
+    public class EmployeeQueryDto : PaginationQueryDto
+    {
+        public string? Search { get; set; }
+        public int? CompanyId { get; set; }
+        public string? OrderBy { get; set; }
+    }
+} 
