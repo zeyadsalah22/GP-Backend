72728d5f0f04013bd636255a3a708f9f99feeb6c
Ahmed-waled <103792966+Ahmed-waled@users.noreply.github.com>
2025-06-08 15:37:19 +0300

    nothing

--- GPBackend/GPBackend/obj/Debug/net8.0/GPBackend.AssemblyInfo.cs
+++ GPBackend/GPBackend/obj/Debug/net8.0/GPBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GPBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80ee687522c626e06dd16de0543ed8f3135af1b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b42ffa38e0e4b2dfc35450361e30eb668ebb7e23")]
 [assembly: System.Reflection.AssemblyProductAttribute("GPBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GPBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
