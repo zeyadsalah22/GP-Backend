c3b6dbaad440fb1799201d642861d75dcd7a671a
Ahmed-waled <103792966+Ahmed-waled@users.noreply.github.com>
2025-10-21 01:18:11 +0300

    Added Notification Pagination DTO, and Repository logic to handle database operations

---
+++ GPBackend/GPBackend/Repositories/Interfaces/INotificationRepository.cs
@@ -0,0 +1,21 @@
+using GPBackend.Models.Enums;
+using GPBackend.Models;
+using GPBackend.DTOs.Notification;
+using GPBackend.DTOs.Common;
+
+namespace GPBackend.Repositories.Interfaces
+{
+    public interface INotificationRepository
+    {
+        public Task<Notification?> GetByIdAsync(int userId, int notificationId);
+        public Task<Notification> CreateAsync(Notification notification);
+        public Task<bool> UpdateAsync(Notification notification);
+        public Task<bool> DeleteAsync(int id);
+        public Task<PagedResult<Notification>> GetPagedAsync(int userId, NotificationQueryDto notificationQueryDto);
+        public Task<List<Notification>> GetUnreadAsync(int userId);
+        public Task<int> GetUnreadCountAsync(int userId);
+        public Task<bool> BulkUpdateAsync(List<Notification> notifications);
+        public Task<List<Notification>?> BulkCreateAsync(List<Notification> notifications);
+        public Task<int> BulkDeleteAsync(List<int> ids);
+    }
+}
