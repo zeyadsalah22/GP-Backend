1cca4833dba1527bc7207c34dc0b0aec2505dfa6
Ahmed-waled <103792966+Ahmed-waled@users.noreply.github.com>
2025-10-20 21:58:20 +0300

    Added the Notification Table migration

---
+++ GPBackend/GPBackend/Migrations/20251020185452_Added Notification Table.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GPBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedNotificationTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Notifications",
+                columns: table => new
+                {
+                    notification_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    user_id = table.Column<int>(type: "int", nullable: false),
+                    actor_id = table.Column<int>(type: "int", nullable: false),
+                    type = table.Column<int>(type: "int", nullable: false),
+                    entity_targeted_id = table.Column<int>(type: "int", nullable: true),
+                    message = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    is_read = table.Column<bool>(type: "bit", nullable: false),
+                    created_at = table.Column<DateTime>(type: "datetime2(0)", precision: 0, nullable: false, defaultValueSql: "(sysutcdatetime())"),
+                    is_deleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notifications", x => x.notification_id);
+                    table.ForeignKey(
+                        name: "FK_Notifications_Actor",
+                        column: x => x.actor_id,
+                        principalTable: "Users",
+                        principalColumn: "user_id");
+                    table.ForeignKey(
+                        name: "FK_Notifications_Users",
+                        column: x => x.user_id,
+                        principalTable: "Users",
+                        principalColumn: "user_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notifications_actor_id",
+                table: "Notifications",
+                column: "actor_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notifications_user_id",
+                table: "Notifications",
+                column: "user_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notifications");
+        }
+    }
+}
---
+++ GPBackend/GPBackend/Models/Notification.cs
@@ -0,0 +1,37 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using GPBackend.Models.Enums;
+
+namespace GPBackend.Models
+{
+    public partial class Notification
+    {
+        [Key]
+        public int NotificationId { get; set; }
+        
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        
+        [ForeignKey("Actor")]
+        public int ActorId { get; set; }
+        
+        [Required]
+        public NotificationType Type { get; set; }
+        
+        public int? EntityTargetedId { get; set; }
+        
+        [Required]
+        [StringLength(100)]
+        public required string Message { get; set; }
+        
+        public bool IsRead { get; set; }
+        
+        public DateTime CreatedAt { get; set; }
+        
+        public bool IsDeleted { get; set; }
+
+        // Navigation Properties
+        public virtual User? User { get; set; }
+        public virtual User? Actor { get; set; }
+    }
+}
